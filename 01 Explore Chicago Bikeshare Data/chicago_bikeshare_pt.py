5031b146ae66876412e5184c1888aeed158aeb5c
tl86br <tl86br@gmail.com>
2018-06-19 23:40:37 -0300

    adds task 1 solution

--- 01 Explore Chicago Bikeshare Data/chicago_bikeshare_pt.py
+++ 01 Explore Chicago Bikeshare Data/chicago_bikeshare_pt.py
@@ -42,8 +42,16 @@ input("Aperte Enter para continuar...")
 # TAREFA 1
 # TODO: Imprima as primeiras 20 linhas usando um loop para identificar os dados.
 print("\n\nTAREFA 1: Imprimindo as primeiras 20 amostras")
+
+# DONE TAREFA 1.1: Por For Loop
+for index in range(0,20):
+    pprint(data_list[index])
+
 # Vamos mudar o data_list para remover o cabeçalho dele.
 data_list = data_list[1:]
+
+# DONE TAREFA 1.2: Por List Comprehension
+sample_first_20 = [line for line in data_list[:20]]
 
 # Nós podemos acessar as features pelo índice
 # Por exemplo: sample[6] para imprimir gênero, ou sample[-2]
