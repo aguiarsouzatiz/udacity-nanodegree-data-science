3fe954e2997a10afbc0bd5d2bd97702f0c53b1c3
tl86br <tl86br@gmail.com>
2018-09-23 18:58:52 -0300

    Adds color option to function plot; Adjuts means lines color

--- 02 Data Analysis Titanic Data/data/data_modules/data_plot_functions.py
+++ 02 Data Analysis Titanic Data/data/data_modules/data_plot_functions.py
@@ -1,11 +1,12 @@
 import seaborn as sns
 
-def plot_distribution(data_frame, ax=None):
+def plot_distribution(data_frame, ax=None, color=None):
     '''
     Plots a distribution chart from data frame series
     '''
     ax_order = ax or None
-    return sns.distplot(data_frame, kde=False, ax=ax_order)
+    color_name = color or None
+    return sns.distplot(data_frame, kde=False, ax=ax_order, color=color_name)
 
 def get_means_of(data_frame):
     '''
@@ -15,19 +16,18 @@ def get_means_of(data_frame):
             'median': data_frame.median(), \
             'mode': data_frame.mode().get_values()[0]}
 
-def plot_distribution_with_means(data_frame, ax=None, title=None):
+def plot_distribution_with_means(data_frame, ax=None, color=None, title=None):
     ''''
     Plots a seaborn distribution chart with mean, median and mode mark line
     '''
-    ax_order = ax or None
-    distribution = plot_distribution(data_frame, ax_order)
+    distribution = plot_distribution(data_frame, ax=ax, color=color)
     mean, median, mode = get_means_of(data_frame).values()
     format_2_decimals = lambda number: f'{float(number):.2f}'
     
     distribution.set_title(title.upper())
-    distribution.axvline(mean, color='r', linestyle='-', label=f'mean {format_2_decimals(round(mean, 2))}')
-    distribution.axvline(median, color='b', linestyle='--', label=f'median {format_2_decimals(round(median, 2))}')
-    distribution.axvline(mode, color='g', linestyle='-.', label=f'mode {format_2_decimals(round(mode, 2))}')
+    distribution.axvline(mean, color='#000000', linestyle='-', label=f'mean {format_2_decimals(round(mean, 2))}')
+    distribution.axvline(median, color='#777777', linestyle='--', label=f'median {format_2_decimals(round(median, 2))}')
+    distribution.axvline(mode, color='#555555', linestyle=':', label=f'mode {format_2_decimals(round(mode, 2))}')
     distribution.legend()
     
 def set_title(data_frame_plot, title):
