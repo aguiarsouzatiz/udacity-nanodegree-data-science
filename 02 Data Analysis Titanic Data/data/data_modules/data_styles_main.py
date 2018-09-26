ba3b1f4f40b7e7f1be516c7663dcd7989db33aec
tl86br <tl86br@gmail.com>
2018-09-26 02:27:37 -0300

    Adds styles module

---
+++ 02 Data Analysis Titanic Data/data/data_modules/data_styles_main.py
@@ -0,0 +1,10 @@
+# estilos para cada tipo de dado
+color_survivors = '#28b14e'
+color_victims = '#b12885'
+palette_destiny = {'yes': color_survivors, 'no': color_victims}
+palette_classes = {'first': '#8a6d3b','second': '#7e888a', 'third': '#8e5753'}
+palette_gender = {'male': '#2798de','female': '#de8f27'}
+palette_companion = {'alone': '#8e3e96', 'group': '#a05da7'}
+palette_destiny_classes = {**palette_destiny,**palette_classes}
+palette_destiny_gender = {**palette_destiny,**palette_gender}
+palette_maturity = {'child': '#ccbf00', 'adult': '#777777'}
